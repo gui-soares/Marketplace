a2e728cb17d7e5f889442676eea2790947c27d6d
gui-soares <eng.guilhermemsoares@gmail.com>
2019-05-20 20:15:45 -0300

    Configuração  mongoose + mongodb

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const databaseConfig = require('./config/database')
 
 class App {
   constructor () {
     this.express = express()
     this.isDEV = process.env.NODE_ENV !== 'production'
 
+    this.database()
     this.midlewares()
     this.routes()
+  }
+
+  database () {
+    mongoose.connect(databaseConfig.uri, {
+      useCreateIndex: true,
+      useNewUrlParser: true
+    })
+
+    var db = mongoose.connection
+
+    db.on('error', console.error)
+    db.once('open', () => {
+      console.log('Connected to MongoDB.')
+    })
   }
 
   midlewares () {
